4cdb0cdf24e16235e0088d839e9f5c65de441f2a
Nolan Lawson <nolan.lawson@gmail.com>
2011-06-10 22:14:27 -0400

    add NamePlayersActivity

---
+++ src/com/nolanlawson/keepscore/NamePlayersActivity.java
@@ -0,0 +1,65 @@
+package com.nolanlawson.keepscore;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class NamePlayersActivity extends Activity implements OnClickListener {
+	
+	public static final String EXTRA_NUM_PLAYERS = "numPlayers";
+	
+	private EditText playerOneEdit, playerTwoEdit, playerThreeEdit, playerFourEdit, playerFiveEdit, playerSixEdit;
+	private Button okButton;
+	
+	private int numPlayers;
+	
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+
+		// prevents the soft keyboard from immediately popping up
+		getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+		
+        
+        setContentView(R.layout.name_players);
+        
+        numPlayers = getIntent().getIntExtra(EXTRA_NUM_PLAYERS, 0);
+        
+        setUpWidgets();
+    }
+
+	private void setUpWidgets() {
+		
+		okButton = (Button) findViewById(android.R.id.button1);
+		
+		playerOneEdit = (EditText) findViewById(R.id.edit_player_1);
+		playerTwoEdit = (EditText) findViewById(R.id.edit_player_2);
+		playerThreeEdit = (EditText) findViewById(R.id.edit_player_3);
+		playerFourEdit = (EditText) findViewById(R.id.edit_player_4);
+		playerFiveEdit = (EditText) findViewById(R.id.edit_player_5);
+		playerSixEdit = (EditText) findViewById(R.id.edit_player_6);
+		
+		// get rid of any edit texts that don't fit given the number of players
+		
+		playerThreeEdit.setVisibility(numPlayers >= 3 ? View.VISIBLE : View.GONE);
+		playerFourEdit.setVisibility(numPlayers >= 4 ? View.VISIBLE : View.GONE);
+		playerFiveEdit.setVisibility(numPlayers >= 5 ? View.VISIBLE : View.GONE);
+		playerSixEdit.setVisibility(numPlayers >= 6 ? View.VISIBLE : View.GONE);
+		
+		okButton.setOnClickListener(this);
+		
+	}
+
+	@Override
+	public void onClick(View v) {
+		// ok button clicked
+		
+		
+		
+	}
+}
--- src/com/nolanlawson/keepscore/NewGameActivity.java
+++ src/com/nolanlawson/keepscore/NewGameActivity.java
@@ -1,10 +1,15 @@
 package com.nolanlawson.keepscore;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class NewGameActivity extends Activity {
+public class NewGameActivity extends Activity implements OnClickListener {
 
+	Button twoPlayersButton, threePlayersButton, fourPlayersButton, fivePlayersButton, sixPlayersButton;
 
 	@Override
     public void onCreate(Bundle savedInstanceState) {
@@ -15,6 +20,46 @@ public class NewGameActivity extends Activity {
     }
 
 	private void setUpWidgets() {
+		
+		twoPlayersButton = (Button) findViewById(R.id.button_2_players);
+		threePlayersButton = (Button) findViewById(R.id.button_3_players);
+		fourPlayersButton = (Button) findViewById(R.id.button_4_players);
+		fivePlayersButton = (Button) findViewById(R.id.button_5_players);
+		sixPlayersButton = (Button) findViewById(R.id.button_6_players);
+		
+		for (Button button : new Button[]{twoPlayersButton, threePlayersButton, 
+				fourPlayersButton, fivePlayersButton, sixPlayersButton}) {
+			button.setOnClickListener(this);
+		}
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.button_2_players:
+			startNamePlayersActivity(2);
+			break;
+		case R.id.button_3_players:
+			startNamePlayersActivity(3);
+			break;
+		case R.id.button_4_players:
+			startNamePlayersActivity(4);
+			break;
+		case R.id.button_5_players:
+			startNamePlayersActivity(5);
+			break;
+		case R.id.button_6_players:
+			startNamePlayersActivity(6);
+			break;	
+		}
+	}
+
+	private void startNamePlayersActivity(int numPlayers) {
+		Intent intent = new Intent(this, NamePlayersActivity.class);
+		intent.putExtra(NamePlayersActivity.EXTRA_NUM_PLAYERS, numPlayers);
+		
+		startActivity(intent);
+		
 	}
 
 	
