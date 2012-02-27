2a985d0238c01b5869fce473415fdc7eb829a006
Nolan Lawson <nolan.lawson@gmail.com>
2012-02-27 14:21:56 -0800

    remove "Home" menu button, add "Shuffle" menu button

--- src/com/nolanlawson/keepscore/GameActivity.java
+++ src/com/nolanlawson/keepscore/GameActivity.java
@@ -1,6 +1,7 @@
 package com.nolanlawson.keepscore;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -202,10 +203,8 @@ public class GameActivity extends Activity {
 	    	Intent settingsIntent = new Intent(GameActivity.this, SettingsActivity.class);
 	    	startActivity(settingsIntent);
 	    	break;
-	    case R.id.menu_home:
-	    	Intent homeIntent = new Intent(GameActivity.this, MainActivity.class);
-	    	homeIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-	    	startActivity(homeIntent);
+	    case R.id.menu_randomize:
+	    	showRandomizePlayersDialog();
 	    	break;
 	    case R.id.menu_reset_scores:
 	    	showResetScoresDialog();
@@ -218,6 +217,40 @@ public class GameActivity extends Activity {
 	    	break;
 	    }
 	    return false;
+	}
+
+	private void showRandomizePlayersDialog() {
+		new AlertDialog.Builder(this)
+			.setCancelable(true)
+			.setTitle(R.string.text_confirm)
+			.setMessage(R.string.text_players_will_be_randomized)
+			.setNegativeButton(android.R.string.cancel, null)
+			.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+				
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					randomizePlayers();
+				}
+			})
+			.show();
+		
+	}
+
+	private void randomizePlayers() {
+		for (PlayerView playerView : playerViews) {
+			playerView.cancelPendingUpdates(); // get rid of any lingering score badges
+		}
+		
+		Collections.shuffle(playerScores);
+		for (int i = 0; i < playerScores.size(); i++) {
+			playerScores.get(i).setPlayerNumber(i);
+		}
+		
+		setUpWidgets();
+		
+		saveGame(game, true, null);
+		Toast.makeText(this, R.string.toast_players_randomized, Toast.LENGTH_SHORT).show();
+		
 	}
 
 	private void showAddPlayerDialog() {
