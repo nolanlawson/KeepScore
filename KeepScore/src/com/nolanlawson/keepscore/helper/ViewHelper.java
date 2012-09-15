c3c45ae165ce7f5fa381fa610ce4a4e06e51e0a2
Nolan Lawson <nolan.lawson@gmail.com>
2012-09-15 19:35:07 +0200

    more theming, japanese and french translations, update changelog

--- KeepScore/src/com/nolanlawson/keepscore/helper/ViewHelper.java
+++ KeepScore/src/com/nolanlawson/keepscore/helper/ViewHelper.java
@@ -7,7 +7,7 @@ public class ViewHelper {
 	
 	public static TextView createSimpleTextView(Context context, int resId, Object... args) {
 		TextView textView = new TextView(context);
-		textView.setTextColor(context.getResources().getColor(android.R.color.primary_text_dark_nodisable));
+		textView.setTextColor(context.getResources().getColor(android.R.color.primary_text_light_nodisable));
 		textView.setText(String.format(context.getString(resId), args));
 		textView.setPadding(5, 0, 5, 0);
 		return textView;
