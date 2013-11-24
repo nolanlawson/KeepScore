aaa1eee59f6f4f669b7079cb84c882f79c9db4b2
Nolan Lawson <nolan.lawson@gmail.com>
2013-11-24 13:28:34 -0800

    whoops, forgot to implement hashCode() and equals()

--- KeepScore/src/com/nolanlawson/keepscore/db/GameSummary.java
+++ KeepScore/src/com/nolanlawson/keepscore/db/GameSummary.java
@@ -54,6 +54,27 @@ public class GameSummary {
         this.dateSaved = dateSaved;
     }
     
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + id;
+        return result;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        GameSummary other = (GameSummary) obj;
+        if (id != other.id)
+            return false;
+        return true;
+    }
+    
     public static Comparator<GameSummary> byRecentlySaved() {
         return new Comparator<GameSummary>() {
 
